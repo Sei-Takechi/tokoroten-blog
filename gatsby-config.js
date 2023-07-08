dc50697875b8c5213cba407368a8bd4730be505d
tokoroten <sei.takechi@gmail.com>
2023-07-08 15:05:32 +0900

    favicon変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,15 +107,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `tokoroten-blog`,
+        short_name: `tokoroten`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/my-icon.JPG`, // This path is relative to the root of the site.
       },
     },
   ],
